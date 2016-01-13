59948ab6b882a85d71b660f8d975fd914b63beda
Kimberley Cook <kimberleycook@Kimberleys-MacBook-Pro.local>
2016-01-13 23:58:23 +0000

    initial routing

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,67 @@
 var React = require('react-native');
+var Router = require('react-native-router');
+
 var {
-  View, 
+  StyleSheet,
+  View,
   Text, 
-  StyleSheet, 
-  TouchableOpacity, 
-  AppRegistry
+  AppRegistry,
+  TouchableOpacity
 } = React;
 
-class ReactNativeStarter extends React.Component {
-  clickMe() {
-    alert('Hi there!');
-  }
-  render() {
+var HelloPage = React.createClass({
+
+  nextPage: function() {
+    this.props.toRoute({
+      name: "A new screen",
+      component: HelloPage
+    });
+  },
+
+   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.clickMe.bind(this)}>
+        <TouchableOpacity onPress={this.nextPage}>
           <View style={styles.box}>
-            <Text>Hello. Please click me.</Text>
+            <Text>Hello {this.props.name}. Please click me.</Text>
           </View>
         </TouchableOpacity>
       </View>
     );
   }
-}
+
+});
+
+var firstRoute = {
+  name: 'Welcome!',
+  component: HelloPage
+};
+
+var ReactNativeStarter = React.createClass({
+  render() {
+    return (
+      <Router firstRoute = {firstRoute} />
+    )
+  }
+});
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  box: {
-    borderColor: 'red',
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    padding: 10,
-    width: 100,
-    height: 100
-  }
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
-
-var MainComponent = function() {
-  this.render = function() {
-    return <ReactNativeStarter name="Component" />;
-  }
-};
 
 AppRegistry.registerComponent('ReactNativeStarter', () => ReactNativeStarter);
--- readme.md
+++ readme.md
@@ -5,6 +5,9 @@ Starting point for react native apps
 ## Todo:
 
  - [Routing](https://github.com/t4t5/react-native-router)
+
+ Go into ```node_module/react-native-router/node_module/react-native`` and delete the ```react-native``` folder.
+
  - Scripts for renaming the project
  - Basic screens
  - Instructions on how to setup the project in XCode
